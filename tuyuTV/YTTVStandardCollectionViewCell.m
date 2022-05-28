345cde498b158545dfc62cd07fdc61caca889062
lechium <kevin.w.bradley@me.com>
2022-05-28 09:58:22 -0700

    fixed build script so it no longer provides unsigned packages, marqueel label only scrolls when a cell is focused

--- tuyuTV/YTTVStandardCollectionViewCell.m
+++ tuyuTV/YTTVStandardCollectionViewCell.m
@@ -12,31 +12,27 @@
 
 @synthesize title, image, durationLabel;
 
-- (id)initWithFrame:(CGRect)frame
-{
-   
+- (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     [self commonInit];
     return self;
 }
 
-- (void)commonInit
-{
+- (void)commonInit {
     [self layoutIfNeeded];
     [self layoutSubviews];
     [self setNeedsDisplay];
+    self.title.holdScrolling = true;
     self.title.alpha = 1;
     DLog(@"dl: %@", self.durationLabel);
 }
 
 
 
-- (void)didUpdateFocusInContext:(UIFocusUpdateContext *)context withAnimationCoordinator:(UIFocusAnimationCoordinator *)coordinator
-{
-       //    NSLog(@"overLayInfo frame: %@", NSStringFromCGRect(self.overlayView.frame));
+- (void)didUpdateFocusInContext:(UIFocusUpdateContext *)context withAnimationCoordinator:(UIFocusAnimationCoordinator *)coordinator {
+    //    NSLog(@"overLayInfo frame: %@", NSStringFromCGRect(self.overlayView.frame));
     //{{160, 30}, {160, 222}}
-    if ([self isFocused])
-    {
+    if ([self isFocused]) {
         
         self.image.adjustsImageWhenAncestorFocused = true;
         CGRect frame = self.overlayView.frame;
@@ -45,6 +41,8 @@
         frame.size.width = 250;
         self.overlayView.frame = frame;
         self.title.alpha = 1;
+        self.title.holdScrolling = false;
+        [self.title restartLabel];
         self.durationTrailingConstraint.constant = 0;
         self.durationBottomConstraint.constant = -10;
     } else {
@@ -55,27 +53,28 @@
         self.overlayView.frame = frame;
         self.image.adjustsImageWhenAncestorFocused = false;
         self.title.alpha = 1;
-         self.durationTrailingConstraint.constant = 10;
+        self.title.holdScrolling = true;
+        [self.title shutdownLabel];
+        self.durationTrailingConstraint.constant = 10;
         self.durationBottomConstraint.constant = -33;
     }
 }
 
-- (void)layoutSubviews
-{
+- (void)layoutSubviews {
     [super layoutSubviews];
-  
+    
 }
 
-- (void)prepareForReuse
-{
+- (void)prepareForReuse {
     [super prepareForReuse];
+    [self.title shutdownLabel];
     self.title.alpha = 1;
 }
 
-- (void)awakeFromNib
-{
+- (void)awakeFromNib {
     [super awakeFromNib];
-        self.title.alpha = 1;
+    self.title.alpha = 1;
+    self.title.holdScrolling = true;
 }
 
 @end
